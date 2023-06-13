686740796362a310f1cd879c415b2e32329412d1
Olivier@Diogenes <olivier@diogenes.brussels>
2023-06-13 15:27:32 +0200

    Use Gotenberg docker

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # Budibase-pdfgenerator
-This is a readme for your new Budibase plugin.
+This is a proof of concept for an initial integration of Gotenberg (Docker-powered stateless API for PDF files) into Budibase. This is not functional or ready for production use.
+The html generated does not yet include the css style to create the PDF correctly.
+Any ideas or comments to help me make progress are welcome.
 
 # Description
-Generate a PDF file from HTML template
+Generate a PDF file from HTML content.
 
 Find out more about [Budibase](https://github.com/Budibase/budibase).
 
 ## Instructions
+
+You need to setup a Gotenberg container.
+In your docker compose services add:
+```
+gotenberg:
+    image: gotenberg/gotenberg:7
+    environment:
+      DEFAULT_LISTEN_PORT: '3000'
+    ports:
+      - 3000:3000
+```
+
+And add a proxy to your container in nginx:
+```
+location /pdf/ {
+        proxy_pass      http://gotenberg:3000/;
+    }
+```
 
 To build your new  plugin run the following in your Budibase CLI:
 ```
@@ -18,3 +38,4 @@ You can also re-build everytime you make a change to your plugin with the comman
 budi plugins --watch
 ```
 
+Please note that this integration is still in the proof of concept stage and should not be used in a production environment.
--- src/Component.svelte
+++ src/Component.svelte
@@ -1,94 +1,61 @@
 <script>
   import { getContext } from "svelte";
-  import { jsPDF } from "jspdf";
-  import { toPng } from "html-to-image";
-  import html2PDF from 'jspdf-html2canvas';
-
-  export let buttonText;
-
-  let PDFContent;
+  import FormData from "form-data";
+  import { extractStyle } from "style-extractor";
 
   const { styleable } = getContext("sdk");
   const component = getContext("component");
 
+  export let buttonText, filename;
+  let PDFContent;
+
   async function createPDF() {
+    const form = new FormData();
+    let htmlContent = PDFContent.innerHTML;
+    const { html, css } = extractStyle(htmlContent);
+    htmlContent = "<style>" + css + "</style>" + html;
+    const blob = new Blob([htmlContent], { type: "text/html" });
+    
+    form.append("files", blob, "index.html");
+    form.append("paperWidth", "8.27");
+    form.append("paperHeight", "11.7");
+    form.append("marginTop", "0");
+    form.append("marginBottom", "0");
+    form.append("marginLeft", "0");
+    form.append("marginRight", "0");
+    form.append("preferCssPageSize", "false");
+    form.append("printBackground", "true");
+    form.append("landscape", "false");
+    form.append("scale", "1.0");
 
-    html2PDF(PDFContent, {
-    jsPDF: {
-      format: 'a4',
-      useCORS: true,
-    },
-    imageType: 'image/jpeg',
-    output: './pdf/generate.pdf'
-  });
-    // const doc = new jsPDF({ orientation: "p", unit: "mm", format: "a4" });
+    try {
+      const response = await fetch("/pdf/forms/chromium/convert/html", {
+        method: "POST",
+        body: form,
+      });
 
-    // domtoimage
-    //   .toPng(PDFContent)
-    //   .then(function (dataUrl) {
-    //     var img = new Image();
-    //     img.src = dataUrl;
-    //     img.onload = () => {
-    //       const imgProps = doc.getImageProperties(img);
-    //       doc.addImage(img, "PNG", 0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight());
-    //       doc.save("file.pdf");
-    //     };
-
-    //     document.body.appendChild(img);
-    //   })
-    //   .catch(function (error) {
-    //     console.error("oops, somehhhthing went wrong!", error);
-    //   });
-
-    // toPng(PDFContent)
-    //   .then((dataUrl) => {
-    //     const img = new Image();
-    //     img.src = dataUrl;
-
-    //     img.onload = () => {
-    //       const imgProps = doc.getImageProperties(img);
-    //       const pdfWidth = doc.internal.pageSize.getWidth();
-    //       const pdfHeight = doc.internal.pageSize.getHeight();
-    //       const nPages = Math.ceil(imgProps.height / pdfHeight);
-
-    //       const pageCanvas = document.createElement("canvas");
-    //       const pageCtx = pageCanvas.getContext("2d");
-    //       pageCanvas.width = pdfWidth;
-    //       pageCanvas.height = pdfHeight;
-
-    //       for (let page = 0; page < nPages; page++) {
-    //         // Display the page.
-    //         const w = pageCanvas.width;
-    //         const h = pageCanvas.height;
-    //         pageCtx.fillStyle = "white";
-    //         pageCtx.fillRect(0, 0, w, h);
-    //         pageCtx.drawImage(img, 0, page * pdfHeight, w, h, 0, 0, w, h);
-
-    //         // Add the page to the PDF.
-    //         if (page) doc.addPage();
-
-    //         const imgData = pageCanvas.toDataURL(`image/PNG`, 1);
-    //         doc.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-    //       }
-
-    //       //doc.addImage(img, "PNG", 0, 0, pdfWidth, pdfHeight);
-    //       doc.save("file.pdf");
-    //     };
-    //   })
-    //   .catch(function (error) {
-    //     console.error("oops, something went wrong!", error);
-    //   });
+      if (response.ok) {
+        const pdfBlob = await response.blob();
+        const url = URL.createObjectURL(pdfBlob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = filename ?? "generated" + ".pdf";
+        link.click();
+        URL.revokeObjectURL(url);
+      } else {
+        // Handle the error response here
+        console.log("Fetch error", response);
+      }
+    } catch (error) { 
+      // Handle fetch or other error here
+      console.log("Error while generating", error);
+    }
   }
 </script>
 
 <div use:styleable={$component.styles}>
-  <button on:click={createPDF}>{buttonText}</button>
-  <div bind:this={PDFContent} style="background-color:#FFF;width: 210mm;">
+  <button on:click={createPDF}>{buttonText ?? "Generate PDF"}</button>
+  <div bind:this={PDFContent} style="background-color:#FFF;width:210mm;">
     <slot />
   </div>
 </div>
-
-<style>
-  * {
-  } /* empty stylesheet0 solves the problem */
-</style>
